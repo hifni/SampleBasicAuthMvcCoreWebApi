a469640b964f6f5b4ce788d9ea1df7f4d6a81d40
Hifni Nazeer - JKSB <HIFNIN@jkintranet.com>
2020-01-31 18:26:01 +0530

    Basic Authentication Implemented as learnt from https://hintdesk.com/2018/08/10/net-core-web-api-basic-authentication/

---
+++ SampleBasicAuthMvcCoreWebApi/Middleware/Authentication/BasicAuthMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBasicAuthMvcCoreWebApi.Middleware.Authentication
+{
+    /// <summary>
+    /// https://hintdesk.com/2018/08/10/net-core-web-api-basic-authentication/
+    /// </summary>
+    public class BasicAuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _realm;
+
+        public BasicAuthMiddleware(RequestDelegate next, string realm)
+        {
+            _next = next;
+            _realm = realm;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string authHeader = context.Request.Headers["Authorization"];
+            if (authHeader != null && authHeader.StartsWith("Basic "))
+            {
+                // Get the encoded username and password
+                var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
+                // Decode from Base64 to string
+                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                // Split username and password
+                var username = decodedUsernamePassword.Split(':', 2)[0];
+                var password = decodedUsernamePassword.Split(':', 2)[1];
+                // Check if login is correct
+                if (IsAuthorized(username, password))
+                {
+                    await _next.Invoke(context);
+                    return;
+                }
+            }
+            // Return authentication type (causes browser to show login dialog)
+            context.Response.Headers["WWW-Authenticate"] = "Basic";
+            // Add realm if it is not null
+            if (!string.IsNullOrWhiteSpace(_realm))
+            {
+                context.Response.Headers["WWW-Authenticate"] += $" realm=\"{_realm}\"";
+            }
+            // Return unauthorized
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        }
+
+        // Make your own implementation of this
+        public bool IsAuthorized(string username, string password)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var basicAuthUserName = "user1";//config["BasicAuth:UserName"];
+            var basicAuthPassword = "Pass#word1";//config["BasicAuth:Password"];
+            // Check that username and password are correct
+            return username.Equals(basicAuthUserName, StringComparison.InvariantCultureIgnoreCase)
+                   && password.Equals(basicAuthPassword);
+        }
+    }
+}
--- SampleBasicAuthMvcCoreWebApi/Startup.cs
+++ SampleBasicAuthMvcCoreWebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SampleBasicAuthMvcCoreWebApi.Middleware.Authentication;
 
 namespace SampleBasicAuthMvcCoreWebApi
 {
@@ -43,6 +44,7 @@ namespace SampleBasicAuthMvcCoreWebApi
                 app.UseHsts();
             }
 
+            app.UseMiddleware<BasicAuthMiddleware>("auth.me");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
