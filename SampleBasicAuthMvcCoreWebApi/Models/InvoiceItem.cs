849b65a584de0a4fadac3e617bdf2891a46064fe
Hifni Nazeer - JKSB <HIFNIN@jkintranet.com>
2020-01-31 12:45:21 +0530

    Add project files.

---
+++ SampleBasicAuthMvcCoreWebApi/Models/InvoiceItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleBasicAuthMvcCoreWebApi.Models
+{
+    public class InvoiceItem
+    {
+        public int InvoiceId { get; set; }
+        public decimal InvoiceValue { get; set; }
+    }
+}
